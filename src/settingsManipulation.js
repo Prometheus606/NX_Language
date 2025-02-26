d89558cc887bae5fb7a3808161f5a0da39f97bf7
Niklas Beitler <niklas.beitler@duh-group.com>
2025-02-26 14:09:26 +0100

    Macht alle farben anpassbar

--- src/settingsManipulation.js
+++ src/settingsManipulation.js
@@ -6,6 +6,11 @@ function manipulateSettings() {
     const extensionSettings = vscode.workspace.getConfiguration("NX_Postprocessor");
     const MOM_Color = extensionSettings.get("MOM_Color");
     const LIB_Color = extensionSettings.get("LIB_Color");
+    const var_Color = extensionSettings.get("var_Color");
+    const keyword_Color = extensionSettings.get("keyword_Color");
+    const keyword_control_Color = extensionSettings.get("keyword_control_Color");
+    const comment_Color = extensionSettings.get("comment_Color");
+    const quotes_Color = extensionSettings.get("quotes_Color");
   
     const settingValue = {
       textMateRules: [
@@ -19,6 +24,36 @@ function manipulateSettings() {
           scope: "keyword.buffer.tcl",
           settings: {
             foreground: `${LIB_Color}`,
+          },
+        },
+        {
+          scope: "variable.other.tcl",
+          settings: {
+            foreground: `${var_Color}`,
+          },
+        },
+        {
+          scope: "keyword.other.tcl",
+          settings: {
+            foreground: `${keyword_Color}`,
+          },
+        },
+        {
+          scope: "keyword.control.tcl",
+          settings: {
+            foreground: `${keyword_control_Color}`,
+          },
+        },
+        {
+          scope: "comment.line.number-sign.tcl",
+          settings: {
+            foreground: `${comment_Color}`,
+          },
+        },
+        {
+          scope: "string.quoted.double.tcl",
+          settings: {
+            foreground: `${quotes_Color}`,
           },
         },
       ],
